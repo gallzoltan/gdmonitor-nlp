779b0f78a285076768501c4d22beeeae8248c9d0
Gáll Zoltán <gallzoltan@gmail.com>
2023-02-13 16:52:57 +0100

    create config module

---
+++ configuration/src/main/java/hu/gallz/configuration/GdMonitorConfig.java
@@ -0,0 +1,63 @@
+package hu.gallz.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "gdmonitor")
+public class GdMonitorConfig {
+	
+	private Links links = new Links();
+    private String username;
+    private String password;
+	
+	public class Links {
+    	private String rssfeed;
+        private String downpdf;
+        private String appini;
+        
+		public String getRssfeed() {
+			return rssfeed;
+		}
+		public void setRssfeed(String rssfeed) {
+			this.rssfeed = rssfeed;
+		}
+		public String getDownpdf() {
+			return downpdf;
+		}
+		public void setDownpdf(String downpdf) {
+			this.downpdf = downpdf;
+		}
+		public String getAppini() {
+			return appini;
+		}
+		public void setAppini(String appini) {
+			this.appini = appini;
+		}        
+    }
+	
+	public Links getLinks() {
+		return links;
+	}
+
+	public void setLinks(Links links) {
+		this.links = links;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- email-service/src/main/java/hu/gallz/emailservice/EwsService.java
+++ email-service/src/main/java/hu/gallz/emailservice/EwsService.java
@@ -1,12 +1,18 @@
 package hu.gallz.emailservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import hu.gallz.configuration.GdMonitorConfig;
 
 @Service
 public class EwsService {
 	
+	@Autowired
+	private GdMonitorConfig config;
+	
 	public String initService() {
-		return "EWS Service is run ok";
+		return "EWS Service is run ok " + config.getLinks().getAppini();
 	}
 
 }

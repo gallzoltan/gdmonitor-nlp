d2537279b36619e620a0530a88646286c4a43472
Gáll Zoltán <gallzoltan@gmail.com>
2023-05-22 16:57:18 +0200

    test for find keywords in PDF

--- application-service/src/main/java/hu/gallz/appservice/service/GovernmentDecreeService.java
+++ application-service/src/main/java/hu/gallz/appservice/service/GovernmentDecreeService.java
@@ -2,6 +2,8 @@ package hu.gallz.appservice.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -9,9 +11,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import hu.gallz.appservice.util.StringConstants;
+
 @Service
 public class GovernmentDecreeService {
 	private static final Logger logger = LoggerFactory.getLogger(GovernmentDecreeService.class);	
+	
+	public Set<Integer> findKeywords(File pdfFile){
+		Set<Integer> foundPages = new HashSet<Integer>();
+		PDDocument document;
+		int numberOfPages = 0;
+		try {
+			PDFTextStripper stripper = new PDFTextStripper();
+			document = PDDocument.load(pdfFile);			
+	        numberOfPages = document.getNumberOfPages();
+			for (int i = 1; i <= numberOfPages; i++) {
+				stripper.setStartPage(i);
+	            stripper.setEndPage(i);
+	            
+	            String content = stripper.getText(document).toLowerCase();
+				if (content.contains(StringConstants.SEARCH_1) || content.contains(StringConstants.SEARCH_2) || content.contains(StringConstants.SEARCH_3) || content.contains(StringConstants.SEARCH_4)) {
+					foundPages.add(i);
+				}
+			}
+			document.close();
+			return foundPages;
+		} catch (IOException e) {
+			logger.info("Pdf read error: {}", e.getMessage());
+		}
+		return null;
+	}
 	
 	public String extractGovernmentDecree(File pdfFile) {
 		PDDocument document;
@@ -46,11 +75,33 @@ public class GovernmentDecreeService {
 	            }
 	        }
 	        document.close();
-	        return governmentDecreeBuilder.toString();
+	        //return governmentDecreeBuilder.toString();
 		} catch (IOException e) {
 			logger.info("Pdf read error: {}", e.getMessage());
 		}
         
         return String.valueOf(numberOfPages);
 	}
+	
+//	private PDDocument getDocument(File pdfFile) {
+//		PDDocument document = null;
+//		try {
+//			return PDDocument.load(pdfFile);
+//		} catch (IOException e) {
+//			logger.info("Pdf read error: {}", e.getMessage());
+//		}
+//		return document;
+//	}
+//	
+//	private String getPageText(int startOfPage, int endOfPage, PDDocument document) {
+//		try {
+//			PDFTextStripper stripper = new PDFTextStripper();
+//			stripper.setStartPage(startOfPage);
+//            stripper.setEndPage(endOfPage);
+//            return stripper.getText(document);
+//		} catch (IOException e) {
+//			logger.info("Pdf stripper error: {}", e.getMessage());
+//		}
+//		return null;
+//	}
 }
--- application-service/src/test/java/hu/gallz/appservice/service/GovernmentDecreeServiceTest.java
+++ application-service/src/test/java/hu/gallz/appservice/service/GovernmentDecreeServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,5 +22,13 @@ public class GovernmentDecreeServiceTest {
 	public void testThatPdfReadIsOK() {
 		Path path = Paths.get("/home/gallz/Letöltések/MK_23_075.pdf");
 		assertEquals(governmentDecreeService.extractGovernmentDecree(path.toFile()), "29");
+	}
+	
+	@Test
+	public void testThatPdfContentKeyWords() {
+		Path path = Paths.get("/home/gallz/Letöltések/MK_23_075.pdf");
+		Set<Integer> setA = new HashSet<Integer>();
+		setA.add(13);
+		assertEquals(governmentDecreeService.findKeywords(path.toFile()), setA);
 	}
 }
